82c80e63fa56470951a7fdbc65fa9d2539ae554f
Andrew <131409157+ajvanderkolk@users.noreply.github.com>
2024-12-06 02:43:48 -0600

    Add files via upload

---
+++ hw3/hw3.py
@@ -0,0 +1,38 @@
+from scipy.linalg import eigh
+import numpy as np
+import matplotlib.pyplot as plt
+
+def load_and_center_dataset(filename):
+    # Your implementation goes here!
+    raise NotImplementedError
+
+def get_covariance(dataset):
+    # Your implementation goes here!
+    raise NotImplementedError
+
+def get_eig(S, k):
+    # Your implementation goes here!
+    raise NotImplementedError
+
+def get_eig_prop(S, prop):
+    # Your implementation goes here!
+    raise NotImplementedError
+
+def project_image(image, U):
+    # Your implementation goes here!
+    raise NotImplementedError
+
+def display_image(orig, proj):
+    # Your implementation goes here!
+    # Please use the format below to ensure grading consistency
+    # fig, (ax1, ax2) = plt.subplots(figsize=(9,3), ncols=2)
+    # return fig, ax1, ax2
+    raise NotImplementedError
+
+def perturb_image(image, U, sigma):
+    # Your implementation goes here!
+    raise NotImplementedError
+
+def combine_image(image1, image2, U, lam):
+    # Your implementation goes here!
+    raise NotImplementedError
